f408778fdd4a3e6f0a18cd80d9fc3d20bba44079
ju4nko <ju4nko@gmail.com>
2026-06-07 15:52:44 +0200

    Initial Commit

---
+++ EjemploDesktop/EjemploDesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjemploDesktopApp.swift
+//  EjemploDesktop
+//
+//  Created by Juanjo on 07/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct EjemploDesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
